0d02574dbfbfe3317ef8da66c72eca5c12b0ed93
Chiranjeewa Peellawatta <chiranlankeshwara2003@gmail.com>
2024-11-24 14:07:17 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,4 +9,23 @@ export const connectDB = async () => {
   try {    
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}, Database: ${conn.connection.name}`);
+    
+
+    mongoose.connection.on("connected", () => {
+      console.log("Mongoose connected to DB");
+    });
+
+    mongoose.connection.on("error", (err) => {
+      console.error("Mongoose connection error:", err.message);
+    });
+
+    mongoose.connection.on("disconnected", () => {
+      console.log("Mongoose disconnected");
+    });
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);// process code 1 means exit with failure, 0 means success
+  }
+};
+
 
